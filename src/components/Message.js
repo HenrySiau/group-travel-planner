7c21155ab04e5568c0b26792543e8bd5e34b728e
henry <siauhenry@gmail.com>
2018-02-12 15:20:53 -0800

    change name from user to userName

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
 // TODO: props validation 
-const Message = ({ chat, user }) => (
-    <li className={`chat ${user === chat.username ? "right" : "left"}`}>
-        {user !== chat.username
-            && <img src={chat.img} alt={`${chat.username}'s profile pic`} />
+const Message = ({ chat, userName }) => (
+    <li className={`chat ${userName === chat.userName ? "right" : "left"}`}>
+        {userName !== chat.userName
+            && <img src={chat.img} alt={`${chat.userName}'s profile pic`} />
         }
         {chat.content}
     </li>
