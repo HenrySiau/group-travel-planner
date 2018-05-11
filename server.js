ffc507374cfbc1f51f9d4213cd2d0fe2b777801a
henry <siauhenry@gmail.com>
2018-05-11 01:16:49 -0700

    feat: add io instance to route controller
    through application-level middleware

--- server.js
+++ server.js
@@ -7,39 +7,35 @@ var cors = require('cors');
 var models = require('./models');
 var SocketIo = require('socket.io');
 
-const server = express();
+const app = express();
+
+app.use(function (req, res, next) {
+    res.io = io;
+    next();
+});
 
 // enable CORSS(Cross Origin Resource Sharing)
-server.use(cors());
+app.use(cors());
 
 // enable public static files
-server.use(express.static('public'));
+app.use(express.static('public'));
 
 // use body parser so we can get info from POST and/or URL parameters
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // use morgan to log requests to the console
-server.use(morgan('dev'));
+app.use(morgan('dev'));
 
-server.use('/api', apiRouter);
+app.use('/api', apiRouter);
 
-// const socketServer = server.listen(process.env.PORT, 'localhost', function (err) {
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     console.log('server listening on port: %s', process.env.PORT);
-// });
-
-
-// models.sequelize.sync({force: true}).then(() => {
-models.sequelize.sync().then(() => {
-
-
-});
-const socketServer = server.listen(config.port, config.host, () => {
+models.sequelize.sync();
+const server = app.listen(config.port, config.host, () => {
     console.info('Express listening on port', config.port);
 });
-const io = new SocketIo(socketServer, { path: '/api/chat' })
+const io = new SocketIo(server, { path: '/api/chat' });
+
 const socketEvents = require('./socketEvents')(io);
+const message = 'the message';
+
+
